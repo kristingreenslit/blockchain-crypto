534830a64d9eb19c22cb7863d789578265944adf
Kristin Greenslit <kristingreenslit@gmail.com>
2020-12-24 20:51:41 -0500

    Move methods to within Block class

--- block.py
+++ block.py
@@ -1,18 +1,47 @@
-class Block:
-	"""
-	Block: a unit of storage
-	Stores transactions in a blockchain that supports a cryptocurrency
-	"""
-	def __init__(self, data):
-		self.data = data
+import time
 
-	def __repr__(self):
-		return f'Block - data: {self.data}'
+class Block:
+    """
+    Block: a unit of storage
+    Stores transactions in a blockchain that supports a cryptocurrency
+    """
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
+        self.data = data
+
+    def __repr__(self):
+        return (
+            'Block('
+            f'timestamp: {self.timestamp}, '
+            f'last_hash: {self.last_hash}, '
+            f'hash: {self.hash}, '
+            f'data: {self.data})'
+        )
+
+@staticmethod
+def mine_block(last_block, data):
+    """
+    Mine a block based on last_block and data
+    """
+    timestamp.time.time_ns()
+    last_hash = last_block.hash
+    hash = f'{timestamp}-{last_hash}'
+
+    return Block(timestamp, last_hash, hash, data)
+
+@staticmethod
+def genesis():
+    """
+    Init genesis block (first block in the chain)
+    """
+    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
 
 def main():
-    block = Block('foo')
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'foo')
     print(block)
-    print(f'block.py __name__: {__name__}')
 
 if __name__ == '__main__':
     main()
